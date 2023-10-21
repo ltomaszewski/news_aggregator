33a4524340f5daf5c27746f4b163309711eeadf9
Lucas Tomaszewski <luki22@gmail.com>
2023-10-21 12:31:21 +0200

    Added health endpoint; Async queue for save tweets

--- src/application/services/REST/TweetRESTService.ts
+++ src/application/services/REST/TweetRESTService.ts
@@ -14,8 +14,8 @@ export class TweetRESTService {
         app.post(basePath + "/tweet/add", async (req, res) => {
             try {
                 const tweetDTO = TweetDTO.createFromObject(req.body);
-                const result = await this.tweetService.save(tweetDTO);
-                res.status(201).json(result);
+                const result = await this.tweetService.enqueueAndSave(tweetDTO);
+                res.status(201).json({ status: "Item has been enqueued for asynchronous saving" });
             } catch (error: any) {
                 res.status(400).json({ error: error.message });
             }
@@ -25,6 +25,11 @@ export class TweetRESTService {
         app.get(basePath + "/tweet/all", async (req, res) => {
             const allTweets = await this.tweetService.getAll();
             res.json(allTweets);
+        });
+
+        // Health ping to let the caller know the service is up
+        app.get(basePath + "/tweet/ping/health", (req, res) => {
+            res.status(201).json({ status: "success" });
         });
     }
 }
--- src/application/services/TweetService.ts
+++ src/application/services/TweetService.ts
@@ -4,9 +4,24 @@ import { TweetRepository } from "../repositories/TweetRepository";
 
 export class TweetService {
     private tweetRepository: TweetRepository;
+    private queue: TweetDTO[] = []
+    private isProcessing: boolean = false
 
     constructor(repository: TweetRepository) {
         this.tweetRepository = repository;
+        setInterval(async () => {
+            if (this.isProcessing || this.queue.length == 0) {
+                return;
+            }
+            this.isProcessing = true
+            while (this.queue.length > 0) {
+                const item = this.queue.shift()
+                if (item) {
+                    await this.save(item)
+                }
+            }
+            this.isProcessing = false
+        }, 500);
     }
 
     async save(tweetDTO: TweetDTO): Promise<Tweet> {
@@ -19,6 +34,10 @@ export class TweetService {
         const newTweet = Tweet.createFromDTO(tweetDTO, newId);
         await this.tweetRepository.insert(newTweet);
         return newTweet;
+    }
+
+    enqueueAndSave(tweetDTO: TweetDTO) {
+        this.queue.push(tweetDTO);
     }
 
     async remove(id: number): Promise<Tweet> {
